6b9ca29f2691c5e20f7dcc64ca6136269921039b
alpielex <55002559+alpielex@users.noreply.github.com>
2019-11-24 18:00:45 +0300

    Add files via upload

---
+++ 2019.10.03/task10.cpp
@@ -0,0 +1,33 @@
+﻿// Реализовать решето Эратосфена
+
+#include <iostream>
+using namespace std;
+
+void r(bool E[], int n) {
+	int a;
+	for (a = 2; a < n; a++) E[a] = 1;
+	a = 2;
+	int i;
+	while (a * a < n) {
+		i = a * a;
+		if (E[a])
+			while ( i < n) {
+				E[i] = 0;
+				i = i + a;
+			}
+		a++;
+	}
+	for (a = 2; a < n; a++) {
+		if (E[a] == 1) {
+			cout << a << " ";
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	bool* E = new bool[n];
+	r(E, n);
+}
