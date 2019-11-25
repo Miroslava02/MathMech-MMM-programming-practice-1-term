fcf79e3598636632983f0540fd306959df8bc7bb
alpielex <55002559+alpielex@users.noreply.github.com>
2019-11-25 22:34:01 +0300

    Add files via upload

---
+++ 2019.11.06/task24.cpp
@@ -0,0 +1,26 @@
+﻿// Написать программу, находящую разложение натурального числа в сумму натуральных квадратов, с наименьшим числом слагаемых
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool rec(int n) {
+	if (n == 0) {
+		return true;
+	}
+	cout << (int) sqrt(n) << "^2";
+	if (n - (int)sqrt(n) * (int)sqrt(n) != 0) {
+		cout << " + ";
+	}
+	rec(n - (int)sqrt(n) * (int)sqrt(n));
+	return true;
+}
+int main() {
+	int a;
+	cin >> a;
+	cout << a << " = ";
+	rec(a);
+	cout << endl;
+}
