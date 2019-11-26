7cc4108bcbc9cc30607b92fd05a1b4ac27748f48
alpielex <55002559+alpielex@users.noreply.github.com>
2019-11-26 20:40:18 +0300

    Add files via upload

---
+++ 2019.11.06/task24.cpp
@@ -0,0 +1,48 @@
+﻿//Написать программу, находящую разложение натурального числа в сумму натуральных квадратов, с наименьшим числом слагаемых
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void search(int* a, int* x, int n) {
+	x[0] = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j * j <= i; j++) {
+			if (x[i] < x[i - j * j] + 1) {
+				x[i] = x[i];
+			}
+			else {
+				x[i] = x[i - j * j] + 1;
+			}
+			if (x[i] == x[i - j * j] + 1) {
+				a[i] = j;
+			}
+		}
+	}
+}
+
+
+void decomposition(int* a, int n) {
+	if (a[n] != -1) cout << a[n] << "^2";
+	n = n - a[n] * a[n];
+	if (n != 0) {
+		cout << "+";
+		decomposition(a, n);
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int* a = new int[n + 1];
+	int* x = new int[n + 1];
+	for (int i = 0; i < n + 1; i++) {
+		a[i] = -1;
+		x[i] = n;
+	};
+	search(a, x, n);
+	decomposition(a, n);
+}
